96a06895ec528783b21b115e7b35deebe9f1e662
jhonn <jhonn.sotomayor@vallegrande.edu.pe>
2024-06-05 03:34:11 -0500

    Mejoras diseño

--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -6,32 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./inicio.component.css']
 })
 export class InicioComponent implements OnInit {
-  // Código del slider
-  slideIndex = 0;
-  totalSlides = 3; // Ajusta este valor al número total de slides
+  carouselImages = [
+    { src: 'https://www.investopedia.com/thmb/v5K6POcZQXnjy1MhKNSNTghQ6jM=/fit-in/1500x750/filters:format(png):fill(white):max_bytes(150000):strip_icc()/Metamask_logo-aca547fe6081482085662b03e2235f98.jpg', alt: 'Descripción de la imagen 1' },
+    { src: 'https://elasesorfinanciero.com/wp-content/uploads/2018/03/blockchain2.png', alt: 'Descripción de la imagen 2' },
+    // Añade más imágenes aquí
+  ];
 
   constructor() { }
 
-  ngOnInit() {
-    this.autoSlide();
-  }
-
-  // Función para cambiar de slide
-  changeSlide(n: number) {
-    this.slideIndex += n;
-    if (this.slideIndex < 0) {
-      this.slideIndex = this.totalSlides - 1;
-    } else if (this.slideIndex >= this.totalSlides) {
-      this.slideIndex = 0;
-    }
-    const slider = document.querySelector('.slider') as HTMLElement;
-    slider.style.transform = `translateX(-${this.slideIndex * 100}%)`;
-  }
-
-  // Función para pasar automáticamente los slides cada cierto tiempo
-  autoSlide() {
-    setInterval(() => {
-      this.changeSlide(1);
-    }, 5000); // Cambiar cada 5 segundos
+  ngOnInit(): void {
   }
 }
